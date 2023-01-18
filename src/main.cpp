8436e2a6dde6ebe361c6c00b9a7e5a77bac05965
Dima Litvinov <dima.litvinov@skalelabs.com>
2023-01-18 19:42:04 +0000

    Recursive direct-light-only rendering, with all-pixels-scan and gamma correction

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "GeometryFloor.h"
 #include "GeometryOpenSpheres.h"
 
+#include <lighting/lighting.h>
+
 #include <sampling/sampling.h>
 #include "tracer_interfaces.h"
 #include <randf.h>
@@ -20,13 +22,13 @@ using namespace glm;
 using namespace std;
 
 // TODO light_hint is not very good solution!
-float ray_power(const Geometry& geometry, const Lighting& lighting, vec3 origin, vec3 direction, size_t depth=0, bool light_hint=false){
+float ray_power(const Geometry& geometry, const Lighting& lighting, vec3 origin, vec3 direction, size_t depth=0){
 
-    if(depth==4)
+    if(depth==2)
         return 0.0f;
 
     std::optional<surface_intersection> si = geometry.traceRay(origin, direction);
-    std::optional<light_intersection> li   = light_hint ? Lighting::last_sample : lighting.traceRayToLight(origin, direction);
+    std::optional<light_intersection> li   = lighting.traceRayToLight(origin, direction);
 
     // 1 check light hit
     if(li.has_value() && depth>0){
@@ -40,36 +42,47 @@ float ray_power(const Geometry& geometry, const Lighting& lighting, vec3 origin,
     if(!si.has_value())
         return 0.0f;
 
+    assert(si->sdf->full_theoretical_weight >= 0.0f);
+
     // 2 continue to geometry
     //DEBUG for geometry debugging
     //return si->position.y+1.0f;
 
     shared_ptr<const Ddf> light_ddf = lighting.distributionInPoint(si->position);
-    shared_ptr<const Ddf> chained_ddf = chain(light_ddf, si->sdf);
 
     // Mix equally SDF distribution and light*SDF distribution!
     // TODO Mix it with unite()!?
     vec3 new_direction;
-    if(rand()%2==0)
-        new_direction = light_ddf->trySample();
-    else
-        new_direction = si->sdf->trySample();
+    float res = 0.0f;
 
-    if(new_direction == vec3())
-        return 0.0f;            // possible dimming because of this
+    //bool cast_light = rand()%2==0;
 
-    assert(si->sdf->full_theoretical_weight >= 0.0f);
+    new_direction = light_ddf->trySample();
+    // possible dimming because of this
+    if(new_direction != vec3()){
+        // correct by light_ddf distribution!
+        float multiplier = 1.0f/light_ddf->value(new_direction)*si->sdf->value(new_direction);
+        res += multiplier*si->sdf->full_theoretical_weight * ray_power(geometry, lighting, si->position, new_direction, depth+1);
+    }
 
-    return si->sdf->full_theoretical_weight * ray_power(geometry, lighting, si->position, new_direction, depth+1, true);
+//    new_direction = si->sdf->trySample();
+//    if(new_direction != vec3()){
+//        res += si->sdf->full_theoretical_weight * ray_power(geometry, lighting, si->position, new_direction, depth+1);
+//    }
+
+    return isfinite(res)?res:0.0f;
 }
 
 void render(const Scene& scene, RenderPlane& r_plane, size_t n_samples){
     for(size_t sample=0; sample<n_samples; ++sample){
 
-        float x = randf();
-        float y = randf();
-//        for(float y = 0.5f/640; y < 1; y+=1.0f/640){
-//        for(float x = 0.5f/640; x < 1; x+=1.0f/640){
+//        float x = randf();
+//        float y = randf();
+        for(size_t iy = 0; iy < 640; iy++){
+        for(size_t ix = 0; ix < 640; ix++){
+
+        float x = (ix+randf())/640.0f;
+        float y = (iy+randf())/640.0f;
 
         vec3 origin, direction;
         tie(origin, direction) = scene.camera->sampleRay(x, y);
@@ -80,14 +93,13 @@ void render(const Scene& scene, RenderPlane& r_plane, size_t n_samples){
         // TODO investigate why it can be -1e-3
         assert(value > -1e-3);
         value = value >= 0.0f ? value : 0.0f;
+        assert(isfinite(value));
         r_plane.addRay(x, y, value);
 
-        if((sample+1) % 10000 == 0)
-            cout << (sample+1)/1000 << "k / " << n_samples/1000 << "k" << endl;
 
-//        }}// for x y
+        }}// for x y
 
-//        cout << sample << " / " << n_samples << endl;
+        cout << sample << " / " << n_samples << endl;
 
     }// for sample
 }
@@ -96,13 +108,13 @@ int main(){
 
     shared_ptr<CollectionLighting> lighting = make_shared<CollectionLighting>();
 
-    lighting->addPointLight(vec3{0.9f, 0.0f, -0.8f}, 0.1f, 1.0f);
+    //lighting->addPointLight(vec3{0.9f, 0.0f, -0.8f}, 0.1f, 1.0f);
     // TODO Why it has non-proportional power?
-    lighting->addSphereLight(vec3{-0.8f, 0.0f, -0.8f}, 0.1f, 1.0f);
+    //lighting->addSphereLight(vec3{-0.8f, 0.0f, -0.8f}, 0.1f, 1.0f);
     // radiates down
-    lighting->addAreaLight(vec3{-0.1f, +0.8f-0.1f, -0.8f}, vec3(0.0f, 0.0f, -1.0f), vec3{0.0f, 0.2f, 0.0f}, 1.0f);
+    lighting->addAreaLight(vec3{+0.1f, -0.8f-0.1f, -0.15f}, vec3(0.0f, 0.0f, -1.0f), vec3{0.0f, 0.2f, 0.0f}, 1.0f);
     // radiates forward
-    lighting->addAreaLight(vec3{-0.1f, -0.8f, -0.80-0.1f}, vec3(0.0f, 1.0f, 0.0f), vec3{0.0f, 0.0f, 0.2f}, 1.0f);
+    lighting->addAreaLight(vec3{-0.1f, -0.8f, -0.7f-0.1f}, vec3(0.0f, 1.0f, 0.0f), vec3{0.0f, 0.0f, 0.2f}, 1.0f);
 
     //lighting->addOuterLight(10);
 
@@ -115,12 +127,12 @@ int main(){
 
     GridRenderPlane r_plane(640, 640);
 
-    render(scene, r_plane, 50*r_plane.width*r_plane.height);
+    render(scene, r_plane, 2);
 
     cout << "Max value = " << r_plane.max_value << endl;
 
-    //r_plane.smooth(4);
-    //r_plane.computeSmoothedMax(4);
+    //r_plane.smooth(2);
+    r_plane.computeSmoothedMax(5);
 
     cout << "Smoothed max = " << r_plane.max_value << endl;
 
@@ -130,10 +142,11 @@ int main(){
     float gamma = 0.6f;
     for(size_t y = 0; y<r_plane.height; ++y){
         for(size_t x = 0; x<r_plane.width; ++x){
-            int value = pow(r_plane.pixels[y*r_plane.width+x]/r_plane.max_value, gamma)*255;
-            if(value>255)
-                value = 255;
-            fprintf(fp, "%d ", value);
+            float value = r_plane.pixels[y*r_plane.width+x]/r_plane.max_value;
+            if(value>1.0f)
+                value = 1.0f;
+            int ivalue = pow(value, gamma)*255;
+            fprintf(fp, "%d ", ivalue);
         }
         fprintf(fp, "\n");
     }// for y
