158ea2c270a966ead52d1c55e2a639f3d4415473
Dima Litvinov <dima.litvinov@skalelabs.com>
2023-02-21 15:51:43 +0000

    Use small sphere light, infinite rendering, render with depth=4

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@
 #include <memory>
 #include <iostream>
 #include <thread>
+#include <atomic>
 
 #include <cstdio>
 
@@ -59,7 +60,7 @@ float ray_power(const Geometry& geometry, const Lighting& lighting, vec3 origin,
     Ddf* sdf_tmp = si->sdf.get();
 
     unique_ptr<Ddf> light_ddf = lighting.distributionInPoint(si->position);
-    unique_ptr<Ddf> mix_ddf = unite(move(light_ddf), 0.0f, move(si->sdf), 1.0f);
+    unique_ptr<Ddf> mix_ddf = unite(move(light_ddf), 1.0f, move(si->sdf), 1.0f);
 
     vec3 new_direction;
     float res = 0.0f;
@@ -72,18 +73,21 @@ float ray_power(const Geometry& geometry, const Lighting& lighting, vec3 origin,
             float multiplier = 1.0f/mix_ddf->value(new_direction)*sdf_tmp->value(new_direction);
             res += multiplier*si->albedo * ray_power(geometry, lighting, si->position, new_direction, depth+1);
         }
+//        new_direction = si->sdf->trySample();
+//        // possible dimming because of this
+//        if(new_direction != vec3()){
+//            res += si->albedo * ray_power(geometry, lighting, si->position, new_direction, depth+1);
+//        }
     }
     return isfinite(res) ? res/n_rays : 0.0f;
 }
 
-void render(const Scene& scene, RenderPlane& r_plane, size_t n_samples){
+void render_sample(const Scene& scene, RenderPlane& r_plane){
+    //        float x = randf();
+    //        float y = randf();
+    for(size_t iy = 0; iy < 640; iy++){
+    for(size_t ix = 0; ix < 640; ix++){
 
-//        float x = randf();
-//        float y = randf();
-for(size_t iy = 0; iy < 640; iy++){
-for(size_t ix = 0; ix < 640; ix++){
-
-    for(size_t sample=0; sample<n_samples; ++sample){
 
         float x = (ix+randf())/640.0f;
         float y = (iy+randf())/640.0f;
@@ -105,12 +109,10 @@ for(size_t ix = 0; ix < 640; ix++){
         assert(isfinite(value));
         r_plane.addRay(x, y, value);
 
-    }// for sample
-
-}// for x
-cout << iy << endl;
-}// for y
-}//render()
+    }// for x
+    //cout << iy << endl;
+    }// for y
+}//render_sample()
 
 Scene make_scene_box(){
 
@@ -152,7 +154,8 @@ Scene make_scene_smallpt(){
 
     shared_ptr<CollectionLighting> lighting = make_shared<CollectionLighting>();
 
-    lighting->addSphereLight(vec3(50,681.6-.27,81.6f), 600.0f);
+    lighting->addSphereLight(vec3(50,81.6-16.5,81.6), 1.5f);
+    //lighting->addSphereLight(vec3(50,681.6-.27,81.6f), 600.0f);
 
     //lighting->addSphereLight(vec3(50,50,50), 10.0f);
     //lighting->addOuterLight(1e+4);
@@ -194,36 +197,45 @@ int main(int argc, char** argv){
 
     Gui gui;
 
-    std::thread th([&scene, &gui, &r_plane](){
-       render(scene, gui, n_rays);
-       gui.finalize();
-       cout << "Saving to result.png" << endl;
-       gui.save("result.png");
+    std::atomic<bool> termination_requested = false;
+    std::thread th([&scene, &gui, &termination_requested](){
+        // render infinitely
+        for(size_t sample=0;; ++sample){
+            render_sample(scene, gui);
+            cout << "Sample " << (sample+1) << endl;
+            if(termination_requested)
+                break;
+        }// for sample
     });
 
     gui.work();
-    th.detach();
-    //th.join();
 
-    cout << "Max value = " << r_plane.max_value << endl;
+    termination_requested = true;
+    th.join();
 
-    //r_plane.smooth(2);
-    //r_plane.computeSmoothedMax(5);
-    cout << "Smoothed max = " << r_plane.max_value << endl;
+    gui.finalize();
+    cout << "Saving to result.png" << endl;
+    gui.save("result.png");
 
-    FILE* fp = fopen("result.pgm", "wb");
-    fprintf(fp, "P2\n%lu %lu\n%d\n", r_plane.width, r_plane.height, 255);
+//    cout << "Max value = " << r_plane.max_value << endl;
 
-    float contrast = 500;
-    float gamma = 0.6f;
-    for(size_t y = 0; y<r_plane.height; ++y){
-        for(size_t x = 0; x<r_plane.width; ++x){
-            int ivalue = n_val(r_plane.pixels[y*r_plane.width+x], r_plane.max_value, contrast, gamma);
-            fprintf(fp, "%d ", ivalue);
-        }
-        fprintf(fp, "\n");
-    }// for y
-    fclose(fp);
+//    //r_plane.smooth(2);
+//    //r_plane.computeSmoothedMax(5);
+//    cout << "Smoothed max = " << r_plane.max_value << endl;
+
+//    FILE* fp = fopen("result.pgm", "wb");
+//    fprintf(fp, "P2\n%lu %lu\n%d\n", r_plane.width, r_plane.height, 255);
+
+//    float contrast = 500;
+//    float gamma = 0.6f;
+//    for(size_t y = 0; y<r_plane.height; ++y){
+//        for(size_t x = 0; x<r_plane.width; ++x){
+//            int ivalue = n_val(r_plane.pixels[y*r_plane.width+x], r_plane.max_value, contrast, gamma);
+//            fprintf(fp, "%d ", ivalue);
+//        }
+//        fprintf(fp, "\n");
+//    }// for y
+//    fclose(fp);
 
     return 0;
 }
